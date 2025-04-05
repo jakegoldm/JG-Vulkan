529a691d5afb230f9e081b6f59bbd929ad1e1a9d
jakeg <jakeg@JAKELAPTOP>
2025-04-04 17:24:53 -0700

    Add image views

--- Triangle/main.cpp
+++ Triangle/main.cpp
@@ -148,6 +148,7 @@ private:
     std::vector<VkImage> swapChainImages; 
     VkFormat swapChainImageFormat; 
     VkExtent2D swapChainExtent; 
+    std::vector<VkImageView> swapChainImageViews; 
     
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
         VkDebugUtilsMessageSeverityFlagBitsEXT severity,
@@ -481,6 +482,34 @@ private:
         swapChainExtent = extent; 
     }
 
+    void createImageViews() {
+        size_t imageCount = swapChainImages.size(); 
+        swapChainImageViews.resize(imageCount); 
+
+        for (size_t i = 0; i < imageCount; i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO; 
+            createInfo.image = swapChainImages[i]; 
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; 
+            createInfo.format = swapChainImageFormat; 
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY; 
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT; 
+            createInfo.subresourceRange.baseMipLevel = 0; 
+            createInfo.subresourceRange.levelCount = 1; 
+            createInfo.subresourceRange.baseArrayLayer = 0; 
+            createInfo.subresourceRange.layerCount = 1; 
+
+            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to create image view."); 
+            }
+        }
+    }
+
     void initWindow() {
         glfwInit(); 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); 
@@ -495,6 +524,7 @@ private:
         pickPhysicalDevice(); 
         createLogicalDevice(); 
         createSwapChain(); 
+        createImageViews(); 
     }
 
     void mainLoop() {
@@ -504,6 +534,9 @@ private:
     }
 
     void cleanup() {
+        for (auto imageView : swapChainImageViews) {
+            vkDestroyImageView(device, imageView, nullptr); 
+        }
         if (enableValidationLayers) {
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
         }
